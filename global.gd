02038d273260bebd1fa18caff6a77041e64eb426
ZetCipher <numbah90@gmail.com>
2023-09-21 01:26:30 -0400

    Set window stretch mode to "Viewport" (temporary)

--- global.gd
+++ global.gd
@@ -5,5 +5,5 @@ const BASE_RESOLUTION := Vector2i(416, 240)
 var window_scale := 3
 
 func _ready():
-	#get_window().size = BASE_RESOLUTION * window_scale
-	pass
+	get_window().size = BASE_RESOLUTION * window_scale
+
